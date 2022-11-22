d253eec8c70c21d2e7a65d3b7d847ea219716a57
Tian23 <tian23@illinois.edu>
2022-11-22 17:09:11 -0600

    add local scheduler part and logs

--- IDunnOo/local_scheduler.py
+++ IDunnOo/local_scheduler.py
@@ -1,13 +1,73 @@
+from worker import Worker
+import zerorpc
+from multiprocessing import Pool
+import logging
+
+
+LOCAL_SCHEDULER_PORT = 6666
+MAX_SLAVE_NUM = 4
+SLAVE_NUM = 2
+
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s.%(msecs)03d %(levelname)s {%(module)s} [%(funcName)s] %(message)s',
+                    datefmt='%Y-%m-%d,%H:%M:%S',
+                    filename='local_scheduler.log',
+                    filemode='w')
+
+
 class LocalScheduler:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, worker_num) -> None:
+        """Initial a Worker object.
+
+        Args:
+            woreker_num (int): The number of worker processes run in local.
+        
+        Returns:
+            None
+
+        """
+        self.slaves = [Worker() for _ in range(worker_num)]
+        self.rpc_server = zerorpc.Server(self)
+        self.rpc_server.bind("tcp://0.0.0.0:{}".format(LOCAL_SCHEDULER_PORT))
+        self.rpc_client = zerorpc.Client()
 
 
-    def assign_task() -> None:
+    def assign_task(self) -> None:
         pass
 
     
-    def spill_task() -> None:
+    def spill_task(self) -> None:
         pass
 
     
+    def run_worker(self) -> None:
+        pass
+
+    def run(self) -> None:
+        """Run the local scheduler process.
+
+        Args:
+            None
+            
+        Returns:
+            None
+
+        """
+        logging.info("Local scheduler starting...")
+        pool = Pool(processes = MAX_SLAVE_NUM)
+        for slave in self.slaves:
+            pool.apply_async(slave.run, ())
+        logging.info("{} worker processes started.".format(len(self.slaves)))
+        
+        logging.info("Local scheduler rpc service started.")
+        self.rpc_server.run()
+        
+        pool.close()
+        pool.join()
+        logging.info("Close local scheduler.")
+
+
+if __name__ == '__main__':
+    ls = LocalScheduler(SLAVE_NUM)
+    ls.run()
--- IDunnOo/worker.py
+++ IDunnOo/worker.py
@@ -1,9 +1,19 @@
 import zerorpc
 import time
 import random
+import logging
+
 
 WORKER_PORT = 5555
 LOCAL_SCHEDULER_PORT = 6666
+
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s.%(msecs)03d %(levelname)s {%(module)s} [%(funcName)s] %(message)s',
+                    datefmt='%Y-%m-%d,%H:%M:%S',
+                    filename='worker.log',
+                    filemode='w')
+
 
 class Worker:
     def __init__(self) -> None:
@@ -22,7 +32,15 @@ class Worker:
 
     
     def __sleep(self):
-        # Sleep random time (0-1s) to avoid conflict.
+        """Sleep random time (0-1s) to avoid conflict.
+
+        Args:
+            None
+        
+        Returns:
+            None
+
+        """
         sleep_t = random.random()
         time.sleep(sleep_t)
 
@@ -44,7 +62,8 @@ class Worker:
                 self.rpc_client.connect("tcp://127.0.0.1:{}".format(LOCAL_SCHEDULER_PORT))
                 model = self.rpc_client.load_model(model_name)
                 break
-            except:
+            except Exception as e:
+                logging.error("Train: Problem happens when load model: ".format(str(e)))
                 self.__sleep()
             
         while True:
@@ -54,6 +73,7 @@ class Worker:
                 dataset = self.rpc_client.load_data(train_data)
                 break
             except:
+                logging.error("Train: Problem happens when load data: ".format(str(e)))
                 self.__sleep()
 
         # Utilize model to train dataset.
@@ -65,6 +85,7 @@ class Worker:
                 self.rpc_client.save(res)
                 break
             except:
+                logging.error("Train: Problem happens when save model: ".format(str(e)))
                 self.__sleep()
         
         return True
@@ -88,6 +109,7 @@ class Worker:
                 model = self.rpc_client.load_model(model_name)
                 break
             except:
+                logging.error("Predict: Problem happens when load model: ".format(str(e)))
                 self.__sleep()
             
         while True:
@@ -97,6 +119,7 @@ class Worker:
                 dataset = self.rpc_client.load_data(data)
                 break
             except:
+                logging.error("Predict: Problem happens when load data: ".format(str(e)))
                 self.__sleep()
 
         # Utilize model to predict the result of dataset.
@@ -108,6 +131,7 @@ class Worker:
                 self.rpc_client.save(res)
                 break
             except:
+                logging.error("Predict: Problem happens when utilize model to predict: ".format(str(e)))
                 self.__sleep()
         
         return True
@@ -123,5 +147,6 @@ class Worker:
             None
 
         """
+        logging.info("A worker started.")
         self.rpc_server.run()
     
