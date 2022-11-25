cba4dc2f2778c2d112e5d0f35deb34007d5c9688
Tian23 <tian23@illinois.edu>
2022-11-24 23:32:52 -0600

    debug

--- IDunnOo/worker.py
+++ IDunnOo/worker.py
@@ -103,7 +103,7 @@ def run() -> None:
         None
     """
     w = Worker(WORKER_PORT)
-    rpc_t = Thread(target = w.start_rpc_service)
+    rpc_t = Thread(target = w.rpc_server.run)
     fd_t = Thread(target = w.fd.run)
 
     rpc_t.start()
