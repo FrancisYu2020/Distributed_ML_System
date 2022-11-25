86bf52af15d3dd9a5ead987195da611716c220f6
Tian23 <tian23@illinois.edu>
2022-11-25 13:49:04 -0600

    debug

--- sdfs/name_node.py
+++ sdfs/name_node.py
@@ -261,7 +261,8 @@ def run(fd):
                     # threads = [threading.Thread(target=listen_ack, args=[id, name_node.done, replicas[i]]) for i, id in enumerate(ids)]
                     # for t in threads:
                     #     t.start()
-                    name_node.ft.insert_file(args[1], replicas)
+                    if args[1] not in name_node.ft.files:
+                        name_node.ft.insert_file(args[1], replicas)
                     data = " ".join(replicas).encode("utf-8")
                     s.sendto(data, client_addr)
                     
