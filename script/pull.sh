12b9c22bdecb73c40327576d15ae441cab7a1c0c
Tian23 <tian23@illinois.edu>
2022-11-29 00:39:03 -0600

    update script

--- script/pull.sh
+++ script/pull.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo VM10 Updating
-ssh tian23@fa22-cs425-2210.cs.illinois.edu "cd mp4-refac; git pull"
+ssh tian23@fa22-cs425-2210.cs.illinois.edu "cd mp4; git pull"
 echo VM10 Updated
 for val in {1..9}
 do
     echo VM$val Updating
-    ssh tian23@fa22-cs425-220$val.cs.illinois.edu "cd mp4-refac; git pull"
+    ssh tian23@fa22-cs425-220$val.cs.illinois.edu "cd mp4; git pull"
     echo VM$val Updated
 done
 
