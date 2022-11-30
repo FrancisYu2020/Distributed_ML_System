b0ee393cd2384597f81738c57da5c72e40ab5db2
Tian23 <tian23@illinois.edu>
2022-11-30 17:09:04 -0600

    update script

--- script/pull.sh
+++ script/pull.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-git add ..
-git commit -m "update code"
-git push origin dnn
-
 echo VM10 Updating
 ssh tian23@fa22-cs425-2210.cs.illinois.edu "cd mp4-dnn; git pull"
 echo VM10 Updated
 for val in {1..9}
 do
     echo VM$val Updating
-    ssh hangy6@fa22-cs425-220$val.cs.illinois.edu "cd mp4-dnn; git pull"
+    ssh tian23@fa22-cs425-220$val.cs.illinois.edu "cd mp4-dnn; git pull"
     echo VM$val Updated
 done
 
