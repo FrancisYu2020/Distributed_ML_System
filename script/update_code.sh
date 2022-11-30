77666111a9f9e88b1694469903f6cd72e5a372b1
Tian23 <tian23@illinois.edu>
2022-11-30 16:21:22 -0600

    del valset

--- script/update_code.sh
+++ script/update_code.sh
@@ -7,7 +7,7 @@ do
     echo VM$val Updated
 done
 echo VM10 Updating
-ssh tian23@fa22-cs425-2210.cs.illinois.edu "rm -rf mp4-dnn; git clone -b dashboard git@gitlab.engr.illinois.edu:hangy6/mp4-hangy6-tian23.git mp4-dnn"
+ssh tian23@fa22-cs425-2210.cs.illinois.edu "rm -rf mp4-dnn; git clone -b dash-board git@gitlab.engr.illinois.edu:hangy6/mp4-hangy6-tian23.git mp4-dnn"
 echo VM10 Updated
 
 echo "-----------All VMs Have Been Updated!------------"
