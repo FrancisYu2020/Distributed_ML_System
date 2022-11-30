0eb81ea16cbc0ab3b10479fe1553e8bf85d1aa09
Tian23 <tian23@illinois.edu>
2022-11-30 14:02:26 -0600

    update gitignore

--- script/pull.sh
+++ script/pull.sh
@@ -5,12 +5,12 @@ git commit -m "update code"
 git push origin dnn
 
 echo VM10 Updating
-ssh hangy6@fa22-cs425-2210.cs.illinois.edu "cd mp4; git pull"
+ssh tian23@fa22-cs425-2210.cs.illinois.edu "cd mp4-dnn; git pull"
 echo VM10 Updated
 for val in {1..9}
 do
     echo VM$val Updating
-    ssh hangy6@fa22-cs425-220$val.cs.illinois.edu "cd mp4; git pull"
+    ssh hangy6@fa22-cs425-220$val.cs.illinois.edu "cd mp4-dnn; git pull"
     echo VM$val Updated
 done
 
