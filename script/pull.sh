2d0bd5c6c487b37b8d896ffa9641ed423c56ae58
Tian23 <tian23@illinois.edu>
2022-12-03 18:14:44 -0600

    update gitignore

--- script/pull.sh
+++ script/pull.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-git add /home/$USER/mp4
-git commit -m "commit change"
-git push origin auto
 echo VM10 Updating
-ssh $USER@fa22-cs425-2210.cs.illinois.edu "cd mp4; git checkout auto; git pull origin auto" 
+ssh $USER@fa22-cs425-2210.cs.illinois.edu "cd mp4;  git pull origin auto" 
 echo VM10 Updated
 for val in {1..9}
 do
     echo VM$val Updating
-    ssh $USER@fa22-cs425-220$val.cs.illinois.edu "cd mp4; git checkout auto; git stash; git pull origin auto"
+    ssh $USER@fa22-cs425-220$val.cs.illinois.edu "cd mp4;  git stash; git pull origin auto"
     echo VM$val Updated
 done
 
